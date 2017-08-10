8e98e060db76265f6289da83a7281526b94454d1
Erasmo Soares <erasmosaraujo@gmail.com>
2017-08-10 13:33:04 -0300

    Initial commit

---
+++ spitzer-angular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FaqComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ spitzer-angular/src/app/faq/faq.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map'
+
+@Component({
+  selector: 'app-faq',
+  templateUrl: './faq.component.html',
+  styleUrls: ['./faq.component.sass']
+})
+export class FaqComponent implements OnInit {
+
+  faqs: Array<any>;
+
+  constructor(private http:Http) { 
+
+  		this.http.get('https://jsonplaceholder.typicode.com/posts')
+  		.map(response => response.json())
+  		.subscribe(res => this.faqs = res);
+  }
+
+  ngOnInit() {
+  }
+
+}
